7c325796e82fcb7516c13391a6d67c8d7a6368fb
James Thigpen <james.r.thigpen@gmail.com>
2013-09-15 18:42:00 -0700

    Updated to use database.

--- index.php
+++ index.php
@@ -4,5 +4,19 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+
+<?php include("config.php"); ?>
+<?php
+// Create connection
+$con=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno($con))
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
+
+
  </body>
 </html>
